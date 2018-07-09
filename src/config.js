3b577abb3856a15c9fb958cfdd1be894cb47b0dd
KeepPassionAlways <406142765@qq.com>
2018-07-09 17:38:03 +0800

    update

---
+++ src/config.js
@@ -0,0 +1,86 @@
+export default {
+  base: {
+    title: '基础组件',
+    content: [{
+      name: 'Badge 徽章',
+      path: './example/badge'
+    }, {
+      name: 'Button 按钮',
+      path: './example/btn'
+    }, {
+      name: 'Capsule 胶囊',
+      path: './example/capsule'
+    }, {
+      name: 'Card 卡片',
+      path: './example/card'
+    }, {
+      name: 'Cell 单元格',
+      path: './example/cell'
+    }, {
+      name: 'Helper 基础样式',
+      path: './example/helper'
+    }, {
+      name: 'Icon 图标',
+      path: './example/icon'
+    }, {
+      name: 'Layout 布局',
+      path: './example/layout'
+    }, {
+      name: 'loading 加载',
+      path: './example/loadmore'
+    }, {
+      name: 'Noticebar 通告栏',
+      path: './example/noticebar'
+    }, {
+      name: 'Panel 面板',
+      path: './example/panel'
+    }, {
+      name: 'Popup 弹出层',
+      path: './example/popup'
+    }, {
+      name: 'Select 选择',
+      path: './example/select'
+    }, {
+      name: 'Stepper 计数器',
+      path: './example/stepper'
+    }, {
+      name: 'Steps 步骤条',
+      path: './example/steps'
+    }, {
+      name: 'Switch 开关',
+      path: './example/switch'
+    }, {
+      name: 'Tab 标签',
+      path: './example/tab'
+    }, {
+      name: 'Tag 标记',
+      path: './example/tag'
+    }]
+  },
+  form: {
+    title: '表单',
+    content: [{
+      name: 'Field 输入框',
+      path: './example/field'
+    }]
+  },
+  action: {
+    title: '操作反馈',
+    content: [{
+      name: 'Actionsheet 行动按钮',
+      path: './example/actionsheet'
+    }, {
+      name: 'Dialog 弹出框',
+      path: './example/dialog'
+    }, {
+      name: 'Toast 轻提示',
+      path: './example/toast'
+    }, {
+      name: 'TopTips 顶部提示',
+      path: './example/toptips'
+    }, {
+      name: 'Datetime 时间选择器',
+      path: './example/dialog'
+    }]
+  }
+};
