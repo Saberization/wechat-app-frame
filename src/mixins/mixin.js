fc7758692cd44b755f9d3239f565879c08ccb508
KeepPassionAlways <406142765@qq.com>
2018-07-24 11:27:43 +0800

    update

---
+++ src/mixins/mixin.js
@@ -0,0 +1,33 @@
+import wepy from 'wepy'
+
+export default class Mixin extends wepy.mixin {
+  /**
+   * 判断数组是否是二维数组
+   * @param {any} arr
+   * @return {Boolean} true or false
+   */
+  isMultiArray(arr) {
+      if (Array.isArray(arr)) {
+        for (let i = 0, len = arr.length; i < len; i++) {
+          const item = arr[i];
+
+          if (Array.isArray(item)) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+
+      return false;
+    },
+
+    /**
+     * 判断数据是否是obj
+     * @param {any} data
+     * @return {Boolean} true or false
+     */
+    isObject(data) {
+      return Object.prototype.toString.call(data).includes('Object');
+    }
+}
