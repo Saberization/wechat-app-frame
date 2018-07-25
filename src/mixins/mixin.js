e7d8de96c767abf63a8bd4be8c525be4c1548dfe
KeepPassionAlways <406142765@qq.com>
2018-07-25 15:23:39 +0800

    update

--- src/mixins/mixin.js
+++ src/mixins/mixin.js
@@ -2,11 +2,11 @@ import wepy from 'wepy'
 
 export default class Mixin extends wepy.mixin {
   /**
-   * 判断数组是否是二维数组
-   * @param {any} arr
-   * @return {Boolean} true or false
-   */
-  isMultiArray(arr) {
+     * 判断数组是否是二维数组
+     * @param {any} arr
+     * @return {Boolean} true or false
+     */
+    isMultiArray(arr) {
       if (Array.isArray(arr)) {
         for (let i = 0, len = arr.length; i < len; i++) {
           const item = arr[i];
@@ -20,7 +20,7 @@ export default class Mixin extends wepy.mixin {
       }
 
       return false;
-    },
+    };
 
     /**
      * 判断数据是否是obj
@@ -29,5 +29,18 @@ export default class Mixin extends wepy.mixin {
      */
     isObject(data) {
       return Object.prototype.toString.call(data).includes('Object');
-    }
+    };
+
+    /**
+     * 小于10的前面补0
+     * @param {Number} num 
+     * @return {String} 小于10返回补完0后的值
+     */
+    appendZero(num) {
+      if (num < 10) {
+        return '0' + num;
+      }
+
+      return num;
+    };
 }
